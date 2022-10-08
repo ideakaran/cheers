9720a59bd1441cfbcbf5721bf8f3021db55a543f
KaranPant <kpant321@gmail.com>
2022-10-09 01:07:44 +0545

    [HZ-20] Update font

--- src/components/App/AppStyle.js
+++ src/components/App/AppStyle.js
@@ -3,7 +3,6 @@ import { MEDIA } from "../../utils/constants";
 
 const AppStyle = styled.div`
   box-sizing: border-box;
-  font-family: "proxima nova", "Roboto", sans-serif;
 
   ${MEDIA.desktop} {
     max-width: 1200px;
